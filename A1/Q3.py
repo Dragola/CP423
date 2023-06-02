a1f0d29061ee5c4db84cd06b688b00847877150b
Osaka <97003266+OsakaOK@users.noreply.github.com>
2023-06-02 15:59:26 -0400

    Update Q3.py
    As of now only OR and AND seems to work. OR NOT and AND NOT doesn't seem to work properly

--- A1/Q3.py
+++ A1/Q3.py
@@ -1,55 +1,41 @@
 from Q2 import InvertedIndex
 
-def evaluate_query(x, y):
-    # Convert x to boolean based on the number
-    if x != 0:
-        x = True
+def process_query(query, inverted_index):
+    query = query.strip().split()
+
+    if len(query) != 3:
+        return None
+
+    x = set(inverted_index.indexList.get(query[0], []))
+    operator = query[1]
+    y = set(inverted_index.indexList.get(query[2], []))
+
+    if operator == "OR":
+        result = x.union(y)
+    elif operator == "AND":
+        result = x.intersection(y)
+    elif operator == "AND" and query[2] == "NOT":
+        result = x.difference(y)
+    elif operator == "OR" and query[2] == "NOT":
+        result = x.union(inverted_index.indexList.keys()).difference(y)
     else:
-        x = False
+        return None
 
-    # Convert y to boolean based on the number
-    if y != 0:
-        y = True
-    else:
-        y = False
-
-    # Evaluate x OR y
-    query_or = x or y
-
-    # Evaluate x AND y
-    query_and = x and y
-
-    # Evaluate x AND NOT y
-    query_and_not = x and not y
-
-    # Evaluate x OR NOT y
-    query_or_not = x or not y
-
-    # Return the results
-    return query_or, query_and, query_and_not, query_or_not
+    return result
 
 
 # Inverted index mapping words to numbers
-invertedIndex = InvertedIndex() 
+invertedIndex = InvertedIndex()
 invertedIndex.addIndex("apple", 1)
 invertedIndex.addIndex("banana", 2)
 invertedIndex.addIndex("cat", 3)
+invertedIndex.addIndex("cat", 4)
 invertedIndex.addIndex("dog", 4)
 
 indexList = invertedIndex.indexList
 invertedIndex.printIndexList()
 
-# Test the function
-word_x = input("Enter the value of x (word): ")
-word_y = input("Enter the value of y (word): ")
-
-# Get the corresponding numbers from the inverted index
-x = indexList[word_x]
-y = indexList[word_y]
-
-result_or, result_and, result_and_not, result_or_not = evaluate_query(x, y)
-
-print(f"x OR y: {result_or}")
-print(f"x AND y: {result_and}")
-print(f"x AND NOT y: {result_and_not}")
-print(f"x OR NOT y: {result_or_not}")
+# testing
+user_input = input("Enter the query: ")
+result = process_query(user_input, invertedIndex)
+print(f"Result: {result}")
