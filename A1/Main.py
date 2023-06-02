b5161f3dc1e72da7f19c49e2b06c3560b857928e
Bryan Gadd <x-dragola-x@live.ca>
2023-06-01 23:27:05 -0400

    Added/updated comments in Main + re-ran Q1 (needs further adjustment)

--- A1/Main.py
+++ A1/Main.py
@@ -1,5 +1,6 @@
 from Q1 import preprocess_text
 from Q2 import InvertedIndex
+#from Q3 import ?
 import os
 
 files: list[str] # the files that we index from
@@ -42,19 +43,23 @@ def createInvertedIndex():
         documentId += 1 # increment the document ID for the next file
 
     return invertedIndex
-    
+
+'''
+Function to output the inverted index to verify integrity (DEBUG ONLY)
+'''
 def outputInvertedIndex():
-    # output the inverted index to verify (DEBUG ONLY)
     file = open("./InvertedIndex.txt", "w")
 
-    documentID = 1
+    documentID = 1 # document ID
 
+    # write each file name + document ID to the txt file
     for documentName in files:
         file.write(documentName + "| DocumentID= " + str(documentID) + "\n")
         documentID += 1
 
     file.write("\n\n")
 
+    # write each index and posting set to the txt file
     invertedIndexDictionary = invertedIndex.indexList
     for index in invertedIndexDictionary:
         file.write(index)
@@ -66,7 +71,7 @@ def outputInvertedIndex():
     file.close()
 
 if __name__ == "__main__":
-    # process the input
+    # process the input (need to do)
     '''
     Input sentence: “lion stood thoughtfully for a moment”
     Input operation sequence: [OR, OR, OR]
@@ -84,7 +89,7 @@ if __name__ == "__main__":
     
     # call method to run the query- Q3
 
-    # output results after query is executed
+    # output results after query is executed (need to do)
     '''
     Output:
     Number of matched documents: 270
