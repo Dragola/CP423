e1131cf999b9cdc2bb3c9899893f14241d0b19c2
Osaka <97003266+OsakaOK@users.noreply.github.com>
2023-06-03 16:38:03 -0400

    Update Q3.py

--- A1/Q3.py
+++ A1/Q3.py
@@ -8,7 +8,7 @@ def process_query(query, inverted_index):
     if len(query) < 3 or len(query) % 2 != 1:
         return None
 
-    result = set(inverted_index.indexList.get(query[i + 1], []))
+    result = set(inverted_index.indexList.get(query[0], []))
     i = 1
 
     # Split the query into operators and operands
@@ -18,16 +18,15 @@ def process_query(query, inverted_index):
 
         # Apply the operators accordingly
         if operator == "AND":
+            if query[i + 1] == "NOT":
+                operand = set(inverted_index.indexList.keys()) - operand
             result = result.intersection(operand)
         elif operator == "OR":
+            if query[i + 1] == "NOT":
+                operand = set(inverted_index.indexList.keys()) - operand
             result = result.union(operand)
-        elif operator == "AND" and query[i + 1] == "NOT":
-            result = result.difference(operand)
-        elif operator == "OR" and query[i + 1] == "NOT":
-            result = result.union(inverted_index.indexList.keys()).difference(operand)
         else:
             return None
-
         i += 2
 
     return result
