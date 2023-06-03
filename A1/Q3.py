0d9c116cbac975b81f1f700d322634899c8517cc
Bryan Gadd <x-dragola-x@live.ca>
2023-06-03 17:41:11 -0400

    Fixed Q3 for NOT operator + Refactoring
    Changes:
    - Can now process NOT operator
    - Renamed variables so it's clear what each variable is.
    - Added comments to explain code.

--- A1/Q3.py
+++ A1/Q3.py
@@ -1,33 +1,51 @@
 from Q2 import InvertedIndex
 
-def process_query(query, inverted_index):
+def process_query(query: str, inverted_index: InvertedIndex, totalDocumentID: int):
     # break down query and process
     query = query.strip().split()
 
-    # check input
-    if len(query) < 3 or len(query) % 2 != 1:
+    # check that query is valid (minimum: word | operation | word). Query can have more
+    if len(query) < 3:
         return None
 
-    result = set(inverted_index.indexList.get(query[0], []))
+    # grab the posting list for the first word in the query
+    firstPostingList = set(inverted_index.indexList[query[0]]) 
+    
+    # index in the query list
     i = 1
+
+    # list of all the documentID's (used for NOT operator)
+    totalPostingList = []
+    for j in range(totalDocumentID):
+        totalPostingList.append(j)
 
     # Split the query into operators and operands
     while i < len(query):
         operator = query[i]
-        operand = set(inverted_index.indexList.get(query[i + 1], []))
-
-        # Apply the operators accordingly
-        if operator == "AND":
-            if query[i + 1] == "NOT":
-                operand = set(inverted_index.indexList.keys()) - operand
-            result = result.intersection(operand)
-        elif operator == "OR":
-            if query[i + 1] == "NOT":
-                operand = set(inverted_index.indexList.keys()) - operand
-            result = result.union(operand)
+        
+        # if NOT is used 
+        if query[i + 1].upper() == "NOT":
+            # flip the posting list for the second word
+            secondPostingList = set(totalPostingList) - set(inverted_index.indexList[query[i + 2]])
+            
+            # skip over the word for next part of query
+            i += 3 
+        # if NOT isn't used
         else:
+            # get the posting list for the second word
+            secondPostingList = set(inverted_index.indexList[query[i + 1]])
+
+            # skip over the word for next part of query
+            i += 2 
+        
+        # Apply the operators accordingly
+        if operator.upper() == "AND":
+            result = firstPostingList.intersection(secondPostingList)
+        elif operator.upper() == "OR":
+            result = firstPostingList.union(secondPostingList)
+        else:
+            # if word isn't AND/OR then we should print to indicate the error
             return None
-        i += 2
 
     return result
 
@@ -47,5 +65,5 @@ invertedIndex.printIndexList()
 
 # testing
 user_input = input("Enter the query: ")
-result = process_query(user_input, invertedIndex)
+result = process_query(user_input, invertedIndex, 3)
 print(f"Result: {result}")
