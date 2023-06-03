01bbd3c87b96af1e1abc0e17ce205100e891759f
Osaka <97003266+OsakaOK@users.noreply.github.com>
2023-06-03 16:02:10 -0400

    Update Q3.py, OR, AND works fine. AND NOT, OR NOT still have some issues

--- A1/Q3.py
+++ A1/Q3.py
@@ -1,25 +1,34 @@
 from Q2 import InvertedIndex
 
 def process_query(query, inverted_index):
+    # break down query and process
     query = query.strip().split()
 
-    if len(query) != 3:
+    # check input
+    if len(query) < 3 or len(query) % 2 != 1:
         return None
 
-    x = set(inverted_index.indexList.get(query[0], []))
-    operator = query[1]
-    y = set(inverted_index.indexList.get(query[2], []))
+    result = set(inverted_index.indexList.get(query[i + 1], []))
+    i = 1
 
-    if operator == "OR":
-        result = x.union(y)
-    elif operator == "AND":
-        result = x.intersection(y)
-    elif operator == "AND" and query[2] == "NOT":
-        result = x.difference(y)
-    elif operator == "OR" and query[2] == "NOT":
-        result = x.union(inverted_index.indexList.keys()).difference(y)
-    else:
-        return None
+    # Split the query into operators and operands
+    while i < len(query):
+        operator = query[i]
+        operand = set(inverted_index.indexList.get(query[i + 1], []))
+
+        # Apply the operators accordingly
+        if operator == "AND":
+            result = result.intersection(operand)
+        elif operator == "OR":
+            result = result.union(operand)
+        elif operator == "AND" and query[i + 1] == "NOT":
+            result = result.difference(operand)
+        elif operator == "OR" and query[i + 1] == "NOT":
+            result = result.union(inverted_index.indexList.keys()).difference(operand)
+        else:
+            return None
+
+        i += 2
 
     return result
 
@@ -27,12 +36,14 @@ def process_query(query, inverted_index):
 # Inverted index mapping words to numbers
 invertedIndex = InvertedIndex()
 invertedIndex.addIndex("apple", 1)
+invertedIndex.addIndex("apple", 2)
 invertedIndex.addIndex("banana", 2)
-invertedIndex.addIndex("cat", 3)
-invertedIndex.addIndex("cat", 4)
-invertedIndex.addIndex("dog", 4)
+invertedIndex.addIndex("banana", 3)
+invertedIndex.addIndex("cherry", 1)
+invertedIndex.addIndex("cherry", 3)
+invertedIndex.addIndex("orange", 2)
+invertedIndex.addIndex("berry", 1)
 
-indexList = invertedIndex.indexList
 invertedIndex.printIndexList()
 
 # testing
