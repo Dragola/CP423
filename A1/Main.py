1fae9f0d6a729cf1c5993184e2ecba1fb5d49c4c
Bryan Gadd <x-dragola-x@live.ca>
2023-06-05 11:18:35 -0400

    Trying to get the main file set up, having issues with Q1's output
    - Q1 is returning a set with no elements for the sentence input. I may know why it's happening...

--- A1/Main.py
+++ A1/Main.py
@@ -1,6 +1,6 @@
 from Q1 import preprocess_text
 from Q2 import InvertedIndex
-#from Q3 import ?
+from Q3 import process_query
 import os
 
 files: list[str] # the files that we index from
@@ -47,7 +47,7 @@ def createInvertedIndex():
 '''
 Function to output the inverted index to verify integrity (DEBUG ONLY)
 '''
-def outputInvertedIndex():
+def outputInvertedIndex(invertedIndex: InvertedIndex):
     file = open("./InvertedIndex.txt", "w")
 
     documentID = 1 # document ID
@@ -78,21 +78,56 @@ if __name__ == "__main__":
 
     Expected preprocessed query: “lion OR stood OR thoughtfully OR moment”
     '''
+    # get query
+    user_input_query_number = input("How many queries are you running? ")
+    query_number = int(user_input_query_number)
+    for i in range(query_number):
+        # get the query
+        print("Query #" + str(i) + ":")      
+        user_input_sentence = input("Input Sentence: ")
+        user_input_operation_sequence = input("Input operation sequence: ")
 
-    invertedIndex = createInvertedIndex() # read all the files and create the inverted index
+        # need to processes the input (remove common words)
+        # Ex. “lion stood thoughtfully for a moment” --> “lion OR stood OR thoughtfully OR moment”
+        operators = user_input_operation_sequence.strip().split()
+        preprocessed_sentence = preprocess_text(user_input_sentence) # returning a set with no elements...
 
-    # sort the documentID's
-    # NOTE: docuemntID set becomes a list
-    invertedIndex.sortDocumentIDs()
+        print("preprocessed_sentence from function: ", end="")
+        print(preprocessed_sentence)
 
-    outputInvertedIndex() # DEBUG ONLY
+        preprocessed_query = ""
+        operator_index = 0
+        for word in preprocessed_sentence:
+            text = word + " " + operators[operator_index] + " "
+            preprocessed_query += text
+            operator_index += 1
+
+        print("Expected preprocessed query: " + preprocessed_query)
     
-    # call method to run the query- Q3
+        # invertedIndex = createInvertedIndex() # read all the files and create the inverted index
 
-    # output results after query is executed (need to do)
-    '''
-    Output:
-    Number of matched documents: 270
-    Minimum number of comparisons required: 671
-    List of retrieved document names
-    '''
+        # # sort the documentID's
+        # # NOTE: docuemntID set becomes a list
+        # invertedIndex.sortDocumentIDs()
+
+        # outputInvertedIndex(invertedIndex) # DEBUG ONLY
+        
+        # # call method to run the query- Q3
+        # result = process_query(user_input, invertedIndex, len(files))
+
+
+        # # output results after query is executed (need to do)
+        # '''
+        # Output:
+        # Number of matched documents: 270
+        # Minimum number of comparisons required: 671
+        # List of retrieved document names
+        # '''
+        # print("Number of matched documents: " + str(len(result)))
+        # print("Minimum number of comparisons required: We need to record this")
+        # print("List of retrieved document names")
+
+        # for documentID in result:
+        #     print(files[documentID])
+    
+    
