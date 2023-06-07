9eef46deaeccb4fc0300766b97c88c9aea545971
Bryan Gadd <x-dragola-x@live.ca>
2023-06-06 21:16:47 -0400

    Comment out Q1 check- need to remove whitespace

--- A1/Q1.py
+++ A1/Q1.py
@@ -41,7 +41,7 @@ def preprocess_text(text):
    tokens = [token for token in tokens if len(token) > 1]
 
    # ensure that token stats with a character (TODO- words that have space or such before get lost)
-   tokens = [token for token in tokens if token[0].isalpha()]
+   #tokens = [token for token in tokens if token[0].isalpha()]
 
    # creating set of all the words
    #unique_words = list(tokens)
