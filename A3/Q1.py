d4012c7e22e6cb685f3db9120bedd52bc661436e
Dragola <x-dragola-x@live.ca>
2023-07-17 20:59:47 -0400

    Load webpage from local copy + added missing required library

--- A3/Q1.py
+++ A3/Q1.py
@@ -3,19 +3,26 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import re
 
+# TODO- Un-comment once we're ready to test on the actual web-page
 # Retrieve the specified webpage as raw HTML using the requests library
-url = "https://en.wikipedia.org/wiki/List_of_Canadian_provinces_and_territories_by_historical_population"
-response = requests.get(url)
-html_content = response.content
+#url = "https://en.wikipedia.org/wiki/List_of_Canadian_provinces_and_territories_by_historical_population"
+#response = requests.get(url)
+#html_content = response.content
 
 
 # Decode the HTML into a tree-structured Python object with the BeautifulSoup library
-soup = BeautifulSoup(html_content, 'html.parser')
+# TODO- Un-comment once we're ready to test on the actual web-page
+#soup = BeautifulSoup(html_content, 'html.parser')
 
+# open local copy of html page (so we don't spam the website)
+with open("./List of Canadian provinces and territories by historical population - Wikipedia.html", encoding='utf8') as file:
+    soup = BeautifulSoup(file, 'html.parser')
+
+print("Printing soup.contents: ...\n")
+print(soup.contents)
 
 # Utilize BeautifulSoup to identify and extract only the tables we're interested in
 tables = soup.find_all('table', class_='wikitable')
-
 
 # Merge the tables, sanitize the text, and transform them into a single Python dictionary
 data = {}
@@ -25,7 +32,6 @@ for table in tables:
     df = df.applymap(lambda x: BeautifulSoup(str(x), 'html.parser').get_text())
     data[i] = df.to_dict()
     i+=1
-
 
 # Construct a pandas dataframe out of this dictionary
 dfs = []
