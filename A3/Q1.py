dd0704bba2793be4ad067c9ad9100dbb4255f40e
Dragola <x-dragola-x@live.ca>
2023-07-18 21:11:16 -0400

    Completed Parts 7 and 8

--- A3/Q1.py
+++ A3/Q1.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import re
+import time
+import os
 
 # TODO- Un-comment once we're ready to test on the actual web-page
 # Retrieve the specified webpage as raw HTML using the requests library
@@ -17,9 +19,6 @@ import re
 # open local copy of html page (so we don't spam the website)
 with open("./List of Canadian provinces and territories by historical population - Wikipedia.html", encoding='utf8') as file:
     soup = BeautifulSoup(file, 'html.parser')
-
-print("Printing soup.contents: ...\n")
-print(soup.contents)
 
 # Utilize BeautifulSoup to identify and extract only the tables we're interested in
 tables = soup.find_all('table', class_='wikitable')
@@ -48,3 +47,61 @@ for i in range(2, len(dfs)):
 
 # Final Database
 merged_df
+
+# Part 7- Generate list of all hyperlinks embedded within the tables
+links = soup.find_all("a", {"href": True, "title": True})
+
+link_list = []
+link_names = []
+# iterate over each link
+for data in links:
+    # extract the text
+    text = data['title']
+
+    # strip any whitespace
+    text = text.strip()
+
+    # check if the title associated with the link is in the tables
+    if (text in merged_df['Name'].values):
+        # ensure the link doesn't already exists in the list
+        if (data['href'] not in link_list):
+            # add title to the list
+            link_names.append(text)
+
+            # add link to the list
+            link_list.append(data['href'])
+
+# print the list of links- DEBUG
+print(link_list)
+
+# Part 8- Download webpages from previous step
+# the domain for the URL
+domain = "https://en.wikipedia.org/"
+
+# the folder where the webpages will be downloaded to
+folder = "./webpages"
+
+# create directory for webpages
+if (os.path.exists("./webpages") == False):
+    os.mkdir("./webpages")
+
+i = 0
+# iterate over each link
+for link in link_list:
+
+    # create the URL
+    url = domain + link
+
+    # get the webpage
+    response = requests.get(url)
+
+    # open the file to write
+    output = open(folder + "/" + link_names[i] + ".html", "wb")
+
+    # write the webpage to the file
+    output.write(response.content)
+    output.close()
+    i += 1
+
+    # wait 5 seconds before downloading the next one (don't want to spam the server)
+    time.sleep(5)
